92df60eb1bff9d2169b74322742166117d3fae8a
Rohan <rohanmisal89@gmail.com>
2024-03-07 13:33:30 +0530

    socketmouse fix

---
+++ modules/canvas/components/MouseRender.tsx
@@ -0,0 +1,31 @@
+import { socket } from "@/common/lib/socket";
+import { useEffect, useState } from "react"
+import { SocketMouse } from "./SocketMouse";
+
+
+const MouseRender = () => {
+
+    const [mouses, setMouses] = useState<string[]>([])
+    console.log(mouses);
+
+    useEffect(() => {
+        socket.on("users_in_room", (socketIds) => {
+            const allUsers = socketIds.filter((socketId) => socketId !== socket.id);
+            setMouses(allUsers);
+        });
+
+        return () => {
+            socket.off("users_in_room");
+        };
+    }, []);
+
+    return (
+        <>
+            {mouses.map((socketId) => {
+                <SocketMouse socketId={socketId} key={socketId} />
+            })}
+        </>
+    )
+}
+
+export default MouseRender
